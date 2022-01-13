96c1f79438e8aa9afe28adf7616508f362bea883
EmmanuelHillary <emmanuelhillaryugwu@gmail.com>
2022-01-13 22:44:50 +0100

    updated code

--- SFL/settings.py
+++ SFL/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'SFL.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': str(os.getenv('DB_NAME')), 
-        'USER': str(os.getenv('DB_USER')), 
-        'PASSWORD': str(os.getenv('DB_PASSWORD')),
-        'HOST': str(os.getenv('DB_HOST')), 
-        'PORT': str(os.getenv('DB_PORT')),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': str(os.getenv('DB_NAME')), 
+#         'USER': str(os.getenv('DB_USER')), 
+#         'PASSWORD': str(os.getenv('DB_PASSWORD')),
+#         'HOST': str(os.getenv('DB_HOST')), 
+#         'PORT': str(os.getenv('DB_PORT')),
+#     }
+# }
 
 
 REST_FRAMEWORK = {
