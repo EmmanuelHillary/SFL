52cde454dd4489f6fa297b30443d32ae98a3adad
EmmanuelHillary <emmanuelhillaryugwu@gmail.com>
2022-01-14 07:45:19 +0100

    updated code

--- SFL/settings.py
+++ SFL/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'SFL.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': str(os.getenv('DB_NAME')), 
-#         'USER': str(os.getenv('DB_USER')), 
-#         'PASSWORD': str(os.getenv('DB_PASSWORD')),
-#         'HOST': str(os.getenv('DB_HOST')), 
-#         'PORT': str(os.getenv('DB_PORT')),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': str(os.getenv('DB_NAME')), 
+        'USER': str(os.getenv('DB_USER')), 
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': str(os.getenv('DB_HOST')), 
+        'PORT': str(os.getenv('DB_PORT')),
+    }
+}
 
 
 REST_FRAMEWORK = {
@@ -176,7 +176,7 @@ REST_FRAMEWORK = {
 
 LOGIN_URL= '/login/'
 
-# CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_TIMEZONE = 'Africa/Lagos'
 CELERY_BEAT_SCHEDULE = {
     "scheduled_task":{
