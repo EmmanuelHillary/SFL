ba767f2fad14ad1685ce73a614ecb3502f5a3aeb
EmmanuelHillary <emmanuelhillaryugwu@gmail.com>
2022-01-17 11:47:59 +0100

    updated code

--- league/admin.py
+++ league/admin.py
@@ -2,9 +2,26 @@ from django.contrib import admin
 from .models import Team, Fixture, Position, Player, GameweekName, GameweekFixtures
 
 
-admin.site.register(Team)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'team', 'sfl_gw_point', 'sfl_total_points', 'total_goal_scored', 'total_assists')
+    list_editable = ('sfl_gw_point', 'total_goal_scored', 'total_assists')
+    search_fields = ['name',]
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+    extra = 0
+    list_display = ('name', 'team', 'sfl_gw_point', 'sfl_total_points', 'total_goal_scored', 'total_assists')
+    list_editable = ('sfl_gw_point', 'total_goal_scored', 'total_assists')
+    search_fields = ['name',]
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [PlayerInline]
+    search_fields = ['name',]
+
+
+admin.site.register(Team, TeamAdmin)
 admin.site.register(Fixture)
 admin.site.register(Position)
-admin.site.register(Player)
+admin.site.register(Player, PlayerAdmin)
 admin.site.register(GameweekName)
 admin.site.register(GameweekFixtures)
